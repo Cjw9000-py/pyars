b7b3f571a1764df549f0b539af4507cba26b1bfb
Cjw <77492076+Cjw9000-py@users.noreply.github.com>
2025-06-23 22:12:57 +0200

    chore: remove outdated comment block

--- pyars/__init__.py
+++ pyars/__init__.py
@@ -19,34 +19,3 @@ __all__ = [
     'Arguments',
     'Path',
 ]
-
-# can you make this example more professional?
-# please keep the pyars module and use its contents exactly like in this example.
-# but change the example so that more and new arguments are demonstrated and
-# the example itself seems more realistic
-#
-# from __future__ import annotations
-# from pyars import *
-# from attrs import define, field
-# from pathlib import Path
-# @arguments
-# class BuildArguments:
-#     projects: set[str] = positional(nargs='+', help='The projects you want to use')
-#     root: Path = flag(extra_opts='--root-path', default='cwd', help='The root dir to use')
-#     cool: bool = switch(help_suffix='global coolness!')
-# @arguments
-# class CleanArguments:
-#     ...
-# @arguments
-# class ConsoleArguments:
-#     root: int
-#     command: Arguments = command(
-#         build=BuildArguments,
-#         clean=CleanArguments,
-#         extra={
-#             'your-mom': BuildArguments
-#         }
-#     )
-# print(ConsoleArguments.parse_args([
-#     '69', 'build', 'proj1', 'plcl', '--root', 'my-root', '-h'
-# ]))
